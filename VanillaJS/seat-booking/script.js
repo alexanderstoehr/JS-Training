9bd3aecf156a5151512c7e465f13ae1355056fb0
orx <tools@eboosting.net>
2023-12-02 00:27:53 +0100

    Code Review

--- VanillaJS/seat-booking/script.js
+++ VanillaJS/seat-booking/script.js
@@ -3,7 +3,6 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-
 let ticketPrice = +movieSelect.value;
 
 populateUi();
