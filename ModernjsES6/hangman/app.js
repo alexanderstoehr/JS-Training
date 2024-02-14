0a52fabbc5eeaeb73e644ec2e39a78c7e8adc19e
orx <tools@eboosting.net>
2024-02-14 09:53:08 +0100

    added classes, cleaned code

--- ModernjsES6/hangman/app.js
+++ ModernjsES6/hangman/app.js
@@ -1,4 +1,13 @@
-const product = {
-    name: "Influence"
-}
-console.log(product.hasOwnProperty("toString")) 
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const game1 = new Hangman('Cat', 2)
+
+puzzleEl.textContent = game1.getPuzzle()
+guessesEl.textContent = game1.getStatusMessage()
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.getPuzzle()
+    guessesEl.textContent = game1.getStatusMessage()
+})
